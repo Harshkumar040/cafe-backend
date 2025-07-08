096a3e7f2eec6073e55bed8d1dbefa79c262a11e
Harshkumar040 <harshkumar040304@gmail.com>
2025-07-08 15:40:57 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const DBUSER = encodeURIComponent(process.env.DBUSER);
+const DBPASS = encodeURIComponent(process.env.DBPASS);
+
+mongoose.connect(
+  `mongodb+srv://${DBUSER}:${DBPASS}@cluster0.n9ugke5.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+).then(() => {
+  console.log(" MongoDB connected");
+  app.listen(8080, () => {
+    console.log("Server started on port 8080");
+  });
+}).catch((err) => {
+  console.error("MongoDB connection error:", err.message);
+});
+
+app.use("/api/users", userRouter);
