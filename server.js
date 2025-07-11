c63e3be12043f781a4b55c059f9e04abbe1db9e8
Harshkumar040 <harshkumar040304@gmail.com>
2025-07-11 15:37:05 +0530

    update

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ const DBUSER = encodeURIComponent(process.env.DBUSER);
 const DBPASS = encodeURIComponent(process.env.DBPASS);
 
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
-
-
-mongoose.connect(
-  `mongodb+srv://${DBUSER}:${DBPASS}@cluster0.n9ugke5.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-).then(() => {
-  console.log(" MongoDB connected");
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
   app.listen(8080, () => {
-    console.log("Server started on port 8080");
+    console.log("Server started");
   });
-}).catch((err) => {
-  console.error("MongoDB connection error:", err.message);
 });
+
+
+// mongoose.connect(
+//   `mongodb+srv://${DBUSER}:${DBPASS}@cluster0.n9ugke5.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+// ).then(() => {
+//   console.log(" MongoDB connected");
+//   app.listen(8080, () => {
+//     console.log("Server started on port 8080");
+//   });
+// }).catch((err) => {
+//   console.error("MongoDB connection error:", err.message);
+// });
 
 app.use("/api/users", userRouter);
